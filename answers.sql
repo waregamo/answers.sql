4cc30fbac6da0fcc5d1379e74bc92cee0e5ff365
waregamo <waregamoses20@gmail.com>
2025-03-14 18:44:17 +0300

    Initial commit: Added answers.sql with SQL queries

---
+++ answers.sql
@@ -0,0 +1,5 @@
+--Create a new database called salesDB
+CREATE DATABASE salesDB; -- This command creates a new database named 'salesDB'.
+
+--  Drop (delete) the database called demo
+DROP DATABASE demo; -- This command deletes the existing database named 'demo'.
